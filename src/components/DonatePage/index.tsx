05c05621ee4136953d1fddb0424bbb1c7ab69627
AH-dark <ahdark@outlook.com>
2022-07-11 20:40:43 +0800

    Feat: Global style supplement, add top bar

--- src/components/DonatePage/index.tsx
+++ src/components/DonatePage/index.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./style.module.scss";
 import Layout from "components/Layout";
 import { Container, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useParams } from "react-router-dom";
-import { useGetDonateInfoQuery } from "../../services/api";
+import { useGetDonateInfoQuery } from "services/api";
 import { useTranslation } from "react-i18next";
-import DonateCard from "../DonateCard";
+import DonateCard from "components/DonateCard";
+import clsx from "clsx";
+import { useAppDispatch } from "redux/hooks";
+import { setTitle } from "redux/reducer/viewUpdate";
 
 const DonatePage: React.FC = () => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
 
     const { id } = useParams<{ id: string }>();
     const { data } = useGetDonateInfoQuery(id);
+
+    useEffect(() => {
+        dispatch(setTitle(t("{{name}} 专属乞讨页", { ns: "title", name: data?.name })));
+    }, [data, dispatch, t]);
 
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
     return (
-        <Layout className={classes.root}>
+        <Layout className={clsx("donatePage", classes.root)}>
             <Container maxWidth={"md"} className={classes.container}>
                 <Typography
                     variant={isMobile ? "h4" : "h3"}
                     component={"h1"}
-                    className={classes.title}
+                    className={clsx("title", classes.title)}
                 >
                     {t("快给 {{name}} 打钱！", {
                         name: data?.name,
--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -5,10 +5,12 @@ import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import { useAppSelector } from "redux/hooks";
 import { useGetSiteInfoQuery } from "services/api";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
 
 const Navbar: React.FC = () => {
     const { data: siteInfo } = useGetSiteInfoQuery();
     const { t } = useTranslation();
+    const history = useHistory();
 
     const viewTitle = useAppSelector((state) => state.viewUpdate.title);
     const title = useMemo(() => {
@@ -19,10 +21,15 @@ const Navbar: React.FC = () => {
         }
     }, [siteInfo, viewTitle, t]);
 
+    const handleClick: React.MouseEventHandler = (e) => {
+        e.preventDefault();
+        history.push("/");
+    };
+
     return (
-        <AppBar position={"static"} className={classes.appbar}>
+        <AppBar position={"fixed"} color={"default"} className={classes.appbar}>
             <Toolbar>
-                <IconButton size={"medium"} color={"inherit"}>
+                <IconButton size={"medium"} color={"inherit"} onClick={handleClick}>
                     <HomeRoundedIcon />
                 </IconButton>
                 <Typography variant={"h6"} component={"span"} className={classes.title}>
